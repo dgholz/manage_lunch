e78160fb6daac7d471e6728694179469b49724c9
Daniel George Holz <dgholz@gmail.com>
2018-01-10 23:03:26 +0000

    Let's get munching

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='manage_lunch',
+    version='1.0',
+    description='Buy Me Lunch',
+    url='https://github.com/dgholz/manage_lunch',
+    packages=find_packages(),
+    install_requires=[],
+)
